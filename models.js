d2c85049c1ae87a993d05fda577ad04f49679f0a
TheMadCoder1999 <160189908+TheMadCoder1999@users.noreply.github.com>
2024-06-19 18:38:49 -0400

    fixed capitalization error

--- models.js
+++ models.js
@@ -16,7 +16,7 @@ let movieSchema = mongoose.Schema({
     Featured: Boolean
 });
 
-let userSchema = Mongoose.Schema ({
+let userSchema = mongoose.Schema ({
     Username: {type: String, required: true},
     Password: {type: String, required: true},
     Email: {type: String, required: true},
