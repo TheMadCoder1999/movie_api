e052db953bad5e74310c35e19a757c6c8d88c398
TheMadCoder1999 <160189908+TheMadCoder1999@users.noreply.github.com>
2024-06-30 15:30:41 -0400

    changed the port for the project

--- index.js
+++ index.js
@@ -283,6 +283,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server and listen for requests on port 8080
-app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
