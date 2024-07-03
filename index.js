845bc3ad10b3ca90dcc7d4b96d67e4f2606b478a
TheMadCoder1999 <160189908+TheMadCoder1999@users.noreply.github.com>
2024-07-02 21:48:56 -0400

    minor change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Directors = Models.Director;
 
 // mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express');
 const morgan = require('morgan');
